29c0df4b61f85c60ca3a5e3d7f773ae8d50cc8e7
Sansinee <gift.chill.chill@gmail.com>
2022-07-26 10:53:25 +0700

    add

--- web/nav.php
+++ web/nav.php
@@ -1,30 +1,25 @@
-<div class="w3-main">
-        <div class="w3-teal">
-            <button class="w3-button w3-teal w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
-            <div class="">
-                <section class="header">
-                    <a href="user.php" class="logo">engineer portfoilo</a>
-                    <nav class="navbar">
-                        <a href="https://engineer.wu.ac.th/?page_id=15080" target="_blank">หลักสูตร</a>
-                        <a href="https://engineer.wu.ac.th/?page_id=6767" target="_blank">บุคลากร</a>
-                        <a href="book.php">ติดต่อ</a>
+           
+<section class="header">
+    <a href="user.php" class="logo">engineer portfoilo</a>
+    <nav class="navbar">
+        <a href="https://engineer.wu.ac.th/?page_id=15080" target="_blank">หลักสูตร</a>
+        <a href="https://engineer.wu.ac.th/?page_id=6767" target="_blank">บุคลากร</a>
+        <a href="book.php">ติดต่อ</a>
                     
-                    </nav>
-                    <div id="menu-btn" class="fas fa-bars"></div>
-                </section>
-            </div>
-        </div>
-        <div class="w3-sidebar w3-bar-block w3-collapse w3-card " style="width:250px;" id="mySidebar" >
-            <button class="w3-bar-item w3-button w3-large w3-hide-large" onclick="w3_close()">Close &times;</button>
-            <h5 href="user.php" class="w3-bar-item w3-button">หน้าแรก</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ข้อมูลนักเรียน</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ข้อมูลครอบครัว</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ข้อมูลการศึกษา</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ข้อมูลกิจกรรมและผลงาน</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ตัวอย่าง Portfolio</h5>
-            <h5  href="#" class="w3-bar-item w3-button">Profile</h5>
-            <h5  href="#" class="w3-bar-item w3-button">ออกจากระบบ</h5>
-        </div>
+    </nav>
+    
+    <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+        
+<div class="w3-sidebar w3-bar-block w3-collapse w3-card " style="width:250px;" id="mySidebar" >
+    <h5 href="user.php" class="w3-bar-item w3-button">หน้าแรก</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ข้อมูลนักเรียน</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ข้อมูลครอบครัว</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ข้อมูลการศึกษา</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ข้อมูลกิจกรรมและผลงาน</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ตัวอย่าง Portfolio</h5>
+    <h5  href="#" class="w3-bar-item w3-button">Profile</h5>
+    <h5  href="#" class="w3-bar-item w3-button">ออกจากระบบ</h5>
+</div>
 
        
-    </div>
--- web/user.php
+++ web/user.php
@@ -8,62 +8,130 @@ require_once("header.php");
     <?php
     require_once("nav.php");
     ?>
- 
+
     <div class="w3-container"  style="margin-left:200px">
-        <section class="services">
+            <section class="booking">
+                <h3 >ข้อมูลส่วนตัว</h3>
+                <form action="book_form.php" method="post" class="book-form">
+                    <div class="flex">
+                        <div class="inputBox">
+                            <span>name :</span>
+                            <input type="text" placeholder="enter your name" name="name">
+                        </div>
+                        <div class="inputBox">
+                            <span>email :</span>
+                            <input type="email" placeholder="enter your email" name="email">
+                        </div>
+                        <div class="inputBox">
+                            <span>phone :</span>
+                            <input type="number" placeholder="enter your number" name="phone">
+                        </div>
+                        <div class="inputBox">
+                            <span>address :</span>
+                            <input type="text" placeholder="enter your address" name="address">
+                        </div>
+                        <div class="inputBox">
+                            <span>where to :</span>
+                            <input type="text" placeholder="place you want to visit" name="location">
+                        </div>
+                        <div class="inputBox">
+                            <span>how many :</span>
+                            <input type="number" placeholder="number of guests" name="guests">
+                        </div>
+                        
+                    </div>
+                </form>
 
-            <div class="box-container">
+            </section>
 
-                <div class="box" id="myBtn">
-                    <img src="images/icon-1.png" alt="">
-                    <h3>วิศวกรรมโยธา</h3>
-                </div>
-                
-                <div class="box">
-                    <img src="images/icon-2.png" alt="">
-                    <h3>วิศวกรรมไฟฟ้า</h3>
-                </div>
+            <section class="services">
+                <h3 >PORTFOLIO OF MAJOR</h3>
+                    <div class="box-container">
 
-                <div class="box">
-                    <img src="images/icon-3.png" alt="">
-                    <h3>ปิโตรเคมีและพอลิเมอร์</h3>
-                </div>
-               
+                        <div class="box">
+                            <div id="myBtn">
+                                <img src="images/icon-1.png" alt="">
+                                <h3 >วิศวกรรมโยธา</h3>
+                            </div>
+                            <!-- The Modal -->
+                            <div id="myModal" class="modal">
+                                <!-- Modal content -->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <span class="close">&times;</span>
+                                        <h2>Modal Header</h2>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Some text in the Modal Body</p>
+                                        <p>Some other text...</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <h3>Modal Footer</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="box">
+                            <img src="images/icon-2.png" alt="">
+                            <h3>วิศวกรรมไฟฟ้า</h3>
+                        </div>
 
-            </div>
+                        <div class="box">
+                            <img src="images/icon-3.png" alt="">
+                            <h3>ปิโตรเคมีและพอลิเมอร์</h3>
+                        </div>
 
-            
-            <div class="box-container">
-                <div class="box">
-                    <img src="images/icon-4.png" alt="">
-                    <h3>วิศวกรรมเครื่องกลและหุ่นยนต์</h3>
-                </div>
+                    </div>
+                   
+                    <div class="box-container">
+                        <div class="box">
+                            <img src="images/icon-4.png" alt="">
+                            <h3>วิศวกรรมเครื่องกลและหุ่นยนต์</h3>
+                        </div>
 
-                <div class="box">
-                    <img src="images/icon-5.png" alt="">
-                    <h3>วิศวกรรมคอมพิวเตอร์และปัญญาประดิษฐ์</h3>
-                </div>
+                        <div class="box">
+                            <img src="images/icon-5.png" alt="">
+                            <h3>วิศวกรรมคอมพิวเตอร์และปัญญาประดิษฐ์</h3>
+                        </div>
 
-                <div class="box">
-                    <img src="images/icon-6.png" alt="">
-                    <h3>วิศวกรรมเคมีและเคมีเภสัชกรรม</h3>
-                </div>
+                        <div class="box">
+                            <img src="images/icon-6.png" alt="">
+                            <h3>วิศวกรรมเคมีและเคมีเภสัชกรรม</h3>
+                        </div>
 
-            </div>
+                    </div>
 
             </section>
     </div>
     
 
-    
-    
+
     <script>
-        function w3_open() {
-        document.getElementById("mySidebar").style.display = "block";
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+        modal.style.display = "block";
         }
 
-        function w3_close() {
-        document.getElementById("mySidebar").style.display = "none";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
         }
     </script>
        
